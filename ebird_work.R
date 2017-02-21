eddbe8b3c3669015e361078bc326c2fb3ccb73ff
Michael Mitchell <mmitchell@ducks.org>
2017-02-21 15:36:21 -0600

    Plotting by BCR

--- ebird_work.R
+++ ebird_work.R
@@ -2,6 +2,8 @@ library(ggplot2)
 library(scales)
 # Read in ebird data (16 species)
 ebird <- read.delim("D:/ebird/ebd_US_agwtea1_relNov-2016.txt", sep="\t", header=TRUE, quote = "", stringsAsFactors = FALSE, na.strings=c(""))
+species = "AGWT"
+setwd("D:/ebird")
 
 # Drop extra columns and data
 ebird = subset(ebird, ebird$COUNTRY_CODE == "US")
@@ -43,29 +45,31 @@ ebird$CATEGORY = NULL
 
 # Set date field and divide up by year, month, week for plotting
 ebird$Date = as.Date(ebird$OBSERVATION.DATE, "%Y-%m-%d")
-ebird$Year = format(ebird$Date, "%Y")
-ebird$Month = format(ebird$Date, "%m")
-# Remove years < 2005 and months 5,6,7,8
-ebird = subset(ebird, ebird$Year >= "2005")
-ebird = subset(ebird, ebird$Month >= "09" | ebird$Month <= "04")
+ebird$Year = strtoi(format(ebird$Date, "%Y"))
+ebird$Month = strtoi(format(ebird$Date, "%m"))
+ebird$Day = strtoi(format(ebird$Date, "%d"))
+ebird$Month = ifelse(format(ebird$Date, "%m") == "09", 9, ebird$Month)
+# Remove years < 2005 and months 6,7,8
+ebird = subset(ebird, ebird$Year > 2005 & ebird$Year <= 2016)
+ebird = subset(ebird, ebird$Month >= 9 | ebird$Month <= 4)
 
-ebird$Month = ifelse(ebird$Month == "09", "9", ebird$Month)
-ebird$Winter = ifelse(strtoi(ebird$Month) <= 4, paste(strtoi(ebird$Year) - 1,ebird$Year, sep = "/"),paste(ebird$Year, strtoi(ebird$Year) + 1, sep = "/"))
+ebird$Winter = ifelse(ebird$Month <= 4, paste(ebird$Year - 1,ebird$Year, sep = "/"),paste(ebird$Year, ebird$Year + 1, sep = "/"))
 ebird$Week <- as.Date(cut(ebird$Date, breaks = "week", start.on.monday = FALSE))
-ebird$Month = ifelse(strtoi(ebird$Month) <= 4, substring(ebird$Month,2),ebird$Month)
-
+#ebird$Month = ifelse(ebird$Month <= 5, substring(ebird$Month,2),ebird$Month)
+ebird = subset(ebird, ebird$Winter != "2005/2006" & ebird$Winter != "2016/2017")
 
 # Reorder months
-ebird$Month = factor(ebird$Month, levels=c("9", "10", "11", "12", "1", "2", "3", "4"))
+ebird$Month = factor(ebird$Month, levels=c(9, 10, 11, 12, 1, 2, 3, 4))
 #Set X as na
 ebird$OBSERVATION.COUNT = ifelse(ebird$OBSERVATION.COUNT == "X", 1, ebird$OBSERVATION.COUNT)
 ebird$OBSERVATION.COUNT = as.numeric(ebird$OBSERVATION.COUNT)
 
-aggCount = aggregate(ebird$OBSERVATION.COUNT, list(Month=ebird$Month, Winter=ebird$Winter), sum)
+aggCount = aggregate(ebird$OBSERVATION.COUNT, list(Month=ebird$Month, ebird$BCR.CODE, Winter=ebird$Winter), sum)
 # Graph one winter
-ggplot(data=aggCount, aes(x=Month, y=x, group=Winter, color=Winter)) +
-  geom_line()
-
-# Graph by month
-ggplot(data=ebird, aes(x=Month, y=OBSERVATION.COUNT, group=Winter, color=Winter)) +
-  geom_line()
+plot = ggplot(data=aggCount, aes(x=Month, y=x, group=Winter, color=Winter)) +
+  stat_smooth(se=FALSE, na.rm = TRUE) + 
+  ggtitle(paste("Figure 1. Observation count sum mapped over wintering period for ", species, sep="")) +  
+  facet_wrap(~ Group.2)
+# + scale_y_continuous(labels = comma) # optional code to remove scientific notation
+print(plot)
+ggsave(file=paste(species, "_Pop.jpg",sep=""), plot=plot, width=7, height=4)
